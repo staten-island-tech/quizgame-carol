fdf6794fe3a145529c4687de957b701b5a7753b8
Carol-Student22 <Carol-Student22@users.noreply.github.com>
2021-04-08 23:01:43 -0400

    Figure Stuff Out - Unfinished

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,27 @@
 console.log("connected");
 
+const DOMSelectors = {
+  quizContainer: document.querySelector(".quiz"),
+  submitButton: document.getElementsByClassName("submitBtn"),
+  resultContainer: document.querySelector("#result"),
+  answer: document.querySelectorAll(".answer"),
+  choiceA: document.getElementsByClassName("answerA"),
+  choiceB: document.getElementsByClassName("answerB"),
+  choiceC: document.getElementsByClassName("answerC"),
+  choiceD: document.getElementsByClassName("answerD"),
+};
+
+console.log(DOMSelectors.quizContainer);
+
 //Questions
 const quizQuestions = [
-  {
-    question: "Good Morning!",
-    a: "Good Morning!",
-    b: "Good Morning",
-    c: "*You stare at the stranger",
-    d: "Good Morning",
-    correct: { a, b, d },
-  },
   {
     question: "How are you?",
     a: "...",
     b: "Why should I answer?",
     c: "*You do not respond",
     d: "Good. how are you?",
-    correct: d,
+    correct: "d",
   },
   {
     question:
@@ -25,7 +30,7 @@ const quizQuestions = [
     b: "*You do not respond",
     c: "*You try to remember this kind stranger",
     d: "Eh. Could've been better.",
-    correct: d,
+    correct: "d",
   },
   {
     question:
@@ -34,34 +39,42 @@ const quizQuestions = [
     b: "They've been great!",
     c: "*Should I be concerned?",
     d: "...",
-    correct: b,
+    correct: "b",
   },
   {
     question:
-      "Great to hear! Anyways, gotta run. I'm almost late for my job. Let's meet again some time soon alright!",
+      "Great to hear! Anyways, gotta run. I'm almost late for my job. See you at 5. Your place, alright?",
     a: "Hold up. Who are you?",
     b: "Sure, see you later!",
     c: "*The Stranger seems to know where I live... But who is this stranger?",
-    d: "Huh? First, a family. Then KIDS?! Then MY ADDRESS! Who are you?!",
-    correct: b,
+    d:
+      "Huh? First, a family. Then KIDS?! Then MY ADDRESS! You're not making any sense?",
+    correct: "b",
   },
 ];
 
-//Grab the HTML (DOM)
-
-
-//Set Score Values
-let currentQuestion = {};
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
-const scorePt = 1;
-
-//If Answers are Right/Wrong Functions
-startGame = () => {
-  questionCounter = 0;
-  score = 0;
-  if question
+///Load Questions on screen
+const quiz = function () {
+  quizQuestions.forEach(
+    (item) =>
+      DOMSelectors.quizContainer.insertAdjacentHTML(
+        "beforeend",
+        `<div class="quizBox">
+      <h1 class="question"> ${item.question} </h1>
+      <p class="answer answerA"> ${item.a} </p>
+      <p class="answer answerB"> ${item.b} </p>
+      <p class="answer answerC"> ${item.c} </p>
+      <p class="answer answerD"> ${item.d} </p> </div>`
+      ),
+    console.log(`function "quiz" is complete.`)
+  );
 };
+quiz();
 
-//Result
+//Variables
+let score = 0;
+//Mark which answer choice was chosen - correct or not
+
+//Add points if the answer chosen is correct (activated/used whent he submit btn is pressed)
+
+//Show results at the bottom of the page of the total among of questions correct - followed by a short text
